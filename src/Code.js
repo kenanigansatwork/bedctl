1e20a557ffc39d5f922f42d84d531e711516d4e7
Kenneth Dait <49531569+kpdait@users.noreply.github.com>
2020-10-27 23:31:58 -0500

    added boilerplate QUnit tests for boilerplate function

--- src/Code.js
+++ src/Code.js
@@ -14,7 +14,46 @@
  * @param {Event} e event object describing GET request parameters
  * @returns {HtmlOutput} contains HTML code of specified web page
  */
-const doGet = e => HtmlService.createHtmlOutput("<h1>bedctl</h1>");
+const doGet = e => runQUnit();
+
+function calculateAmountAndQty(quantity,amount){
+    if(typeof (quantity) == 'undefined' || quantity == 'null' || isNaN(quantity))
+    { quantity=0; }
+    if(typeof (amount) == 'undefined' || amount == 'null' || isNaN(amount))
+    { amount=0; }
+    return (quantity * amount);
+}
+
+const runQUnit = () => {
+    QUnit.helpers(this);
+
+    function testFunctions() {
+        testingCalculateAmountAndQty();
+    }
+
+    function testingCalculateAmountAndQty(){
+       QUnit.test( "calculateAmountAndQty testing", function() {
+          expect(7);
+          equal( calculateAmountAndQty(10,2000), 20000, "Test for quantity : 10 and amount : 2000 sp output is 20000" );
+          equal( calculateAmountAndQty("printer",2000), 0, "Test for quantity : printer and amount : 2000 so output is 0" );
+          equal( calculateAmountAndQty(10,"mouse"), 0, "Test for quantity : 10 and amount : mouse so output is 0" );
+          equal( calculateAmountAndQty(10,null), 0, "Test for quantity : 10 and amount : null so output is 0" );
+          equal( calculateAmountAndQty(null,2000), 0, "Test for quantity : null and amount : 2000 so output is 0" );
+          equal( calculateAmountAndQty(undefined,2000), 0, "Test for quantity : undefined and amount : 2000 so output is 0" );
+          equal( calculateAmountAndQty(10,undefined), 0, "Test for quantity : 10 and amount : undefined so output is 0" );
+       });
+    }
+
+    QUnit.config({
+        title: "QUnit for `bedctl` - Test Suite"
+    });
+    QUnit.load(testFunctions);
+    return QUnit.getHtml();
+};
+
+
+
+
 
 
 /**
